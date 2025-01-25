2378f5f4adbafea34ff45be73640eb3a1c2dd0c8
Jakub Slezáček <js@threedragons.net>
2025-01-25 10:32:12 +0100

    Fix walking enemy to bound of walls

--- Assets/Scripts/Enemies/WalkingEnemyController.cs
+++ Assets/Scripts/Enemies/WalkingEnemyController.cs
@@ -5,9 +5,10 @@ public class WalkingEnemyController : EnemyController
 {
     [SerializeField] float width = .5f;
     [SerializeField] float groundDistance = .1f;
+    [SerializeField] float wallDistance = .05f;
     [SerializeField] float speed = 1.0f;
-    Direction currentDirection = Direction.Left;
     [SerializeField, Layer] int groundLayer;
+    Direction currentDirection = Direction.Left;
 
     void FixedUpdate()
     {
@@ -18,7 +19,7 @@ public class WalkingEnemyController : EnemyController
             return;
         }
 
-        if (!CheckGround(currentDirection))
+        if (!CheckGround(currentDirection) || CheckWall(currentDirection))
         {
             currentDirection = currentDirection.Invert();
         }
@@ -30,14 +31,20 @@ public class WalkingEnemyController : EnemyController
     {
         Vector3 leftPosition = transform.position + Vector3.left * width / 2;
         Vector3 rightPosition = transform.position + Vector3.right * width / 2;
+        
         Vector3 groundOffset = Vector3.down * groundDistance;
-
         Gizmos.color = CheckGround(Direction.None) ? Color.green : Color.red;
         Gizmos.DrawLine(transform.position, transform.position + groundOffset);
         Gizmos.color = CheckGround(Direction.Left) ? Color.green : Color.red;
         Gizmos.DrawLine(leftPosition, leftPosition + groundOffset);
         Gizmos.color = CheckGround(Direction.Right) ? Color.green : Color.red;
         Gizmos.DrawLine(rightPosition, rightPosition + groundOffset);
+
+        Vector3 wallOffset = Vector3.right * wallDistance;
+        Gizmos.color = CheckWall(Direction.Left) ? Color.green : Color.red;
+        Gizmos.DrawLine(leftPosition, leftPosition - wallOffset);
+        Gizmos.color = CheckWall(Direction.Right) ? Color.green : Color.red;
+        Gizmos.DrawLine(rightPosition, rightPosition + wallOffset);
     }
 
     /// <summary>
@@ -49,6 +56,15 @@ public class WalkingEnemyController : EnemyController
             transform.position + new Vector3((int)direction * width / 2, 0.01f),
             Vector2.down,
             groundDistance + 0.01f,
+            1 << groundLayer);
+    }
+
+    bool CheckWall(Direction direction)
+    {
+        return Physics2D.Raycast(
+            transform.position + new Vector3((int)direction * (width / 2 - 0.01f), 0.01f),
+            Vector2.right * (int)direction,
+            wallDistance + 0.01f,
             1 << groundLayer);
     }
 }
