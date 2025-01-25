6588308fba2252d1241843b043d4d7eb1572954b
Jakub Slezáček <js@threedragons.net>
2025-01-25 10:48:40 +0100

    Add camera follow

--- Assets/Scripts/RespawnPoint.cs
+++ Assets/Scripts/RespawnPoint.cs
@@ -1,3 +1,4 @@
+using Unity.Cinemachine;
 using UnityEngine;
 
 public class RespawnPoint : MonoBehaviour
@@ -6,6 +7,7 @@ public class RespawnPoint : MonoBehaviour
 
     [SerializeField] private Transform spawnPoint;
     [SerializeField] private Player playerPrefab;
+    [SerializeField] private CinemachineCamera playerFollowCamera;
     public Player Player { get; private set; }
 
     private void Awake()
@@ -33,5 +35,7 @@ public class RespawnPoint : MonoBehaviour
             Destroy(Player.gameObject);
         }
         Player = Instantiate(playerPrefab, spawnPoint.position, Quaternion.identity);
+        if (playerFollowCamera != null)
+            playerFollowCamera.Follow = Player.transform;
     }
 }
