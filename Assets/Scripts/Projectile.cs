91565031ba0124c0eaf28d4e008b267b53458004
Jonáš Rosecký <rosecky.jonas@gmail.com>
2025-01-25 01:26:44 +0100

    Added bubble dying mechanic + allowing players to catch them back

--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 public class Projectile : MonoBehaviour
 {
     public Bubble Bubble { get; private set; }
+    public int bouncesToLive = 1;
+    public float transferBackRatio = 0.5f;
 
     void OnEnable()
     {
@@ -13,5 +15,28 @@ public class Projectile : MonoBehaviour
     void Update()
     {
         
+    }
+
+    // On collision with anything, kill self
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        // Only kill self if the projectile has rigidbody
+        if (Bubble.Rb == null) {
+            return;
+        }
+
+        // If colliding with a bubble, add volume to the bubble
+        Bubble otherBubble = collision.gameObject.GetComponentInChildren<Bubble>();
+        if (otherBubble != null) {
+            otherBubble.volume += Bubble.volume * transferBackRatio;
+            Destroy(gameObject);
+        } else {
+            // If colliding with anything else, reduce bouncesToLive
+            bouncesToLive--;
+
+            if (bouncesToLive < 0) {
+                Destroy(gameObject);
+            }
+        }
     }
 }
